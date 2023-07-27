e47f3b53b4d61be47cd43e83bf5037466e28ce98
fantomcoconut <fantomcoconut@gmail.com>
2023-07-27 20:24:00 +0300

    added files

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        if (section.id === sectionId) {
+            section.classList.add('active');
+        } else {
+            section.classList.remove('active');
+        }
+    });
+}
